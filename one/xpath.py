1662145039580408f0f48c58eb0b58f3bd8282ab
LotteryOne <xuanfengshizi@163.com>
2017-03-05 13:35:58 +0800

    start xpath

--- one/xpath.py
+++ one/xpath.py
@@ -16,6 +16,11 @@ html = '''
         <a id="ddd">BBDFDASd</a>
         <a href="www.baidu.com"></a>
         <a href="www.jikexuyuan.com"></a>
+
+        <div id="test-cccc">div123</div>
+        <div id="test-c1">div456</div>
+        <div id="testbas">div6788</div>
+
     </body>
 
 '''
@@ -28,3 +33,13 @@ for li in content:
 link = select.xpath('//a/@href')
 for l in link:
     print(l)
+
+startStr = select.xpath('//div[starts-with(@id,"test")]/text()')
+
+for sc in startStr:
+    print(sc)
+
+allStr = select.xpath('//ul[@id="unusefuls"]')[0]
+getStr = allStr.xpath('string(.)').replace('\n', '').replace(' ','')
+for get in getStr:
+    print(get)
